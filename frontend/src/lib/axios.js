9b3d7c56764c56ac3d100780d6858c43b915c570
JcodeTeam <pujamasjoo@gmail.com>
2025-04-15 22:31:36 +0700

    edit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://6679-180-243-15-238.ngrok-free.app/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://6679-180-243-15-238.ngrok-free.app/api" : "https://6679-180-243-15-238.ngrok-free.app/api",
   withCredentials: true,
 });
